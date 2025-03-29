94f031bf89d18af2dfc56a64bf7eb20dc0c463bc
Troth99 <trothlol123456@gmail.com>
2025-03-29 08:40:58 +0200

    Add
    Added home page static information, added css to every element

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 
 import "../src/config/firebaseInit"
 import { page } from "./constants/constants.js"
+import { showHomePage } from "./views/home.js"
 import loginView from "./views/login.js"
 
 
-
+page('/', showHomePage)
 page('/login', loginView)
 page.start()
--- src/config/firebaseInit.js
+++ src/config/firebaseInit.js
@@ -1,20 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth , setPersistence, browserLocalPersistence} from "firebase/auth"
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBlfsbUFqpucuVx4Kp2S4XCA0NMlqgc3vU",
-    authDomain: "pettopia-2cc08.firebaseapp.com",
-    projectId: "pettopia-2cc08",
-    storageBucket: "pettopia-2cc08.firebasestorage.app",
-    messagingSenderId: "167658968865",
-    appId: "1:167658968865:web:bc2ab39000782ccdac9060"
+  apiKey: "AIzaSyBlfsbUFqpucuVx4Kp2S4XCA0NMlqgc3vU",
+  authDomain: "pettopia-2cc08.firebaseapp.com",
+  projectId: "pettopia-2cc08",
+  storageBucket: "pettopia-2cc08.firebasestorage.app",
+  messagingSenderId: "167658968865",
+  appId: "1:167658968865:web:bc2ab39000782ccdac9060"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+
+export const db = getFirestore(app)
 
 setPersistence(auth, browserLocalPersistence)
   .then(() => {
